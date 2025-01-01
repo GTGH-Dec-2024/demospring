0f0bd3c5b0dd99113be45385b0c62454843fc5ef
Theodoros Maikantis <teomaik19@gmail.com>
2025-01-01 23:15:45 +0200

    created Book class
    Created a new Book class/model.

---
+++ src/main/java/com/teomaik/demospring/books/Book.java
@@ -0,0 +1,60 @@
+package com.teomaik.demospring.books;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+
+	private String title;
+	private String author;
+	private String publiser;
+	private int publishYear;
+	List<String> theme = new ArrayList<String>();
+	
+	public Book(String title, String author, String publiser, int publishYear, List<String> theme) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.publiser = publiser;
+		this.publishYear = publishYear;
+		this.theme = theme;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getPubliser() {
+		return publiser;
+	}
+	public void setPubliser(String publiser) {
+		this.publiser = publiser;
+	}
+	public int getPublishYear() {
+		return publishYear;
+	}
+	public void setPublishYear(int publishYear) {
+		this.publishYear = publishYear;
+	}
+	public List<String> getTheme() {
+		return theme;
+	}
+	public void setTheme(List<String> theme) {
+		this.theme = theme;
+	}
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", author=" + author + ", publiser=" + publiser + ", publishYear=" + publishYear
+				+ ", theme=" + theme + "]";
+	}
+	
+	
+}
