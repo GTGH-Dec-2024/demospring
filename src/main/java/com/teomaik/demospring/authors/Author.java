a67d5d1f40aa82a08c1b9904b62b91056faae284
Theodoros Maikantis <teomaik19@gmail.com>
2025-01-03 20:04:29 +0200

    Author entity change

--- src/main/java/com/teomaik/demospring/authors/Author.java
+++ src/main/java/com/teomaik/demospring/authors/Author.java
@@ -1,15 +1,42 @@
 package com.teomaik.demospring.authors;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+
+@Entity
+@Table(name = "authors")
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-	private String fisrtName;
-	private String lastName;
-	private String dateOfBirth;
-	
-	public Author(Integer id, String fisrtName, String lastName, String dateOfBirth) {
+
+    @Column(name = "first_name", nullable = true)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = true)
+    private String lastName;
+
+    @Column(name = "date_of_birth", nullable = true)
+    private String dateOfBirth;
+
+    // Default constructor for JPA
+    public Author() {}
+    
+    public Author(String firstName, String lastName, String dateOfBirth) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dateOfBirth = dateOfBirth;
+	}
+    
+	public Author(Integer id, String firstName, String lastName, String dateOfBirth) {
 		this.id = id;
-		this.fisrtName = fisrtName;
+		this.firstName = firstName;
 		this.lastName = lastName;
 		this.dateOfBirth = dateOfBirth;
 	}
@@ -22,11 +49,11 @@ public class Author {
 		this.id = id;
 	}
 
-	public String getFisrtName() {
-		return fisrtName;
+	public String getFirstName() {
+		return firstName;
 	}
-	public void setFisrtName(String fisrtName) {
-		this.fisrtName = fisrtName;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
 	public String getLastName() {
 		return lastName;
--- src/main/java/com/teomaik/demospring/authors/AuthorServices.java
+++ src/main/java/com/teomaik/demospring/authors/AuthorServices.java
@@ -1,54 +1,53 @@
 package com.teomaik.demospring.authors;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class AuthorServices {
 
-	//TODO replace with HashMap
-	List<Author> authors = new ArrayList<Author>();
+	@Autowired AuthorRepository repository;
 
 	//TODO add method getWithID
 	
 	public List<Author> getAllAuthors() {
-		return authors;
+		return repository.findAll();
 	}
 
 	public List<Author> addAuthor(Author author) {
-		int newId = 1;
-		if(authors.size()>0) {
-			newId = authors.get(authors.size()-1).getId() +1;
-		}
-		
-		author.setId(newId);
-		authors.add(author);
-		return authors;
+		repository.save(author);
+		return repository.findAll();
 	}
 
 	public List<Author> removeAuthor(Integer id) {
-		authors.removeIf(author -> author.getId() == id);
-		return authors;
+		repository.deleteById(id);
+		return repository.findAll();
 	}
 
 	public Author updateAuthor(int id, String firstName, String lastName, String dateOfBirth) {
+        Optional<Author> optionalAuthor = repository.findById(id);
+        
+        if (optionalAuthor.isPresent()) {
+            Author existingAuthor = optionalAuthor.get();
+            
+            // Update fields if new values are provided
+            if (firstName != null && !firstName.isEmpty()) {
+                existingAuthor.setFirstName(firstName);
+            }
+            if (lastName != null && !lastName.isEmpty()) {
+                existingAuthor.setLastName(lastName);
+            }
+            if (dateOfBirth != null && !dateOfBirth.isEmpty()) {
+                existingAuthor.setDateOfBirth(dateOfBirth);
+            }
 
-		for (Author author : authors) {
-			if (author.getId() == id) {
-				if (firstName != null)
-					author.setFisrtName(firstName);
-				if (lastName != null)
-					author.setLastName(lastName);
-				if (dateOfBirth != null)
-					author.setDateOfBirth(dateOfBirth);
-
-				return author;
-			}
-		}
-		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Author with id " + id + " doesnt exist");
+            // Save and return the updated author
+            return repository.save(existingAuthor);
+        } else {
+            throw new RuntimeException("Author with ID " + id + " not found");
+        }
 	}
 }
