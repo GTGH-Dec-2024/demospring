a7d0d99412e65d0c7beae31e5e9051ab16b46dc8
Theodoros Maikantis <teomaik19@gmail.com>
2025-01-07 14:17:49 +0200

    example Log

--- src/main/java/com/teomaik/demospring/DemospringApplication.java
+++ src/main/java/com/teomaik/demospring/DemospringApplication.java
@@ -8,6 +8,7 @@ public class DemospringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemospringApplication.class, args);
+		System.out.println("Log: App ready!");
 	}
 
 }
